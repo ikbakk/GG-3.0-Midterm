1395a1da4ab51061f794a85176107cd07770d771
Muhammad Iqbal Firdaus <87456625+ikbakk@users.noreply.github.com>
2023-07-27 12:20:58 +0800

    Delete unused file

--- utils/customError.js
+++
@@ -1,8 +0,0 @@
-const customError = (msg, statusCode, status) => {
-  let error = new Error(msg);
-  error.statusCode = statusCode;
-  error.status = status;
-  return error;
-};
-
-module.exports = customError;
